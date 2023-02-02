4c6e3874861c3ad204c6062027abf24d0c9ebf3c
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-02-02 11:29:11 -0300

    create router regiter

--- app.js
+++ app.js
@@ -7,13 +7,63 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
+// Config JSON response
+app.use(express.json());
+
+const User = require("./models/User");
+
 app.get("/", (req, res) => {
   res.status(200).json({ msg: " welcome to API LOGIN!" });
+});
+
+//Register User
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmpassword } = req.body;
+
+  if (!name) {
+    return res.status(422).json({ msg: "Name is required field." });
+  }
+  if (!email) {
+    return res.status(422).json({ msg: "Email is required field." });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "Pass is required field." });
+  }
+
+  if (password !== confirmpassword) {
+    return res.status(422).json({ msg: "passwords do not match." });
+  }
+
+  const userEXists = await User.findOne({ email: email });
+
+  if (userEXists) {
+    return res.status(422).json({ msg: "E-mail already registered!" });
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  try {
+    await user.save();
+    res.status(201).json({ msg: "User created successfully!" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ msg: "erroThere was a server error, please try again later" });
+  }
 });
 
 // Credencials
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
+
 mongoose
   .connect(
     `mongodb+srv://${dbUser}:${dbPassword}@cluster0.8uscoab.mongodb.net/?retryWrites=true&w=majority`
