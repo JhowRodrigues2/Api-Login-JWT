a4d440d56b47450fc6002b018901d382e8d2f6c4
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-02-02 16:10:23 -0300

    final commit

--- app.js
+++ app.js
@@ -16,6 +16,35 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: " welcome to API LOGIN!" });
 });
 
+//Private Route
+app.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id, "-password");
+  if (!user) {
+    return res.status(404).json({ msg: "User not found." });
+  }
+  res.status(200).json({
+    user,
+  });
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split("")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Access denied." });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Invalid token." });
+  }
+}
+
 //Register User
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmpassword } = req.body;
