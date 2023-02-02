ea6597fa898a50c5f29160bd0ebef5b75dcb288e
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-02-02 14:48:16 -0300

    autentication method

--- app.js
+++ app.js
@@ -60,6 +60,47 @@ app.post("/auth/register", async (req, res) => {
   }
 });
 
+// Login User
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(422).json({ msg: "Email is required field." });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "Pass is required field." });
+  }
+  //check if user exists
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(404).json({ msg: "User not found." });
+  }
+
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Password not found." });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+
+      secret
+    );
+
+    res.status(200).json({ msg: "Successful authentication.", token });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ msg: "erroThere was a server error, please try again later" });
+  }
+});
+
 // Credencials
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
